dbf3e4f99cbfb34cb2381dbdd447a4216b9353e2
Josh Grimaldi <69489227+mabejohnny@users.noreply.github.com>
2020-09-18 10:57:02 -0400

    Added a rungame method which will be my hub for the game, added a method for user to choose single/multiplayer game. next course of action will be to create methods to capture users name, as well a method for multi player users to choose there gesture based off the Gestures List.

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -6,5 +6,24 @@ namespace RPSLS
 {
     class Game
     {
+
+        //member variables
+
+
+
+        //constructor
+        public Game()
+        {
+
+        }
+
+
+
+
+        //member methods
+        public void RunGame()
+        {
+            Console.WriteLine("Welcome To The World Famous" + "\n" + "   Rock!" + "\n" + "      Paper!" + "\n" + "         Scissor!" + "\n" + "            Lizard!" + "\n" + "               Spock!");
+        }
     }
 }
